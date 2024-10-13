73713e8d7e1c89cf6cd38f2a2608bd63b4b809f0
Alkim Ake Gozen <alkimake@pm.me>
2024-10-13 22:41:50 +0900

    darwin: app: alactirry

--- users/ake/darwin/default.nix
+++ users/ake/darwin/default.nix
@@ -7,10 +7,10 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [
-      pkgs.mkalias
-    ];
+  environment.systemPackages = [
+    pkgs.alacritty
+    pkgs.mkalias
+  ];
 
   homebrew = {
     enable = true;
