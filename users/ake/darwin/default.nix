31a69e4dc54a1c410da1c7a185089043e1260398
Alkim Ake Gozen <alkimake@pm.me>
2024-10-13 19:49:00 +0900

    initial commit with darwin

---
+++ users/ake/darwin/default.nix
@@ -0,0 +1,51 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  # nixpkgs.overlays = import ../../lib/overlays.nix ++ [
+  #   (import ./vim.nix { inherit inputs; })
+  # ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+      pkgs.mkalias
+    ];
+
+  homebrew = {
+    enable = true;
+    casks  = [
+      "spotify"
+    ];
+    onActivation.cleanup = "zap";
+  };
+
+
+
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "system-applications";
+      paths = config.environment.systemPackages;
+      pathsToLink = "/Applications";
+    };
+  in
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
+
+  # The user should already exist, but we need to set this up so Nix knows
+  # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
+  users.users.ake = {
+    home = "/Users/ake";
+    shell = pkgs.fish;
+  };
+}
---
+++ users/ake/home-manager/default.nix
@@ -0,0 +1,50 @@
+{ isWSL, inputs, ... }:
+
+{ config, lib, pkgs, ... }:
+
+let
+  sources = import ../../nix/sources.nix;
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+
+  # For our MANPAGER env var
+  # https://github.com/sharkdp/bat/issues/1145
+  manpager = (pkgs.writeShellScriptBin "manpager" (if isDarwin then ''
+    sh -c 'col -bx | bat -l man -p'
+    '' else ''
+    cat "$1" | col -bx | bat --language man --style plain
+  ''));
+in {
+    # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+  
+  xdg.enable = true;
+
+  #---------------------------------------------------------------------
+  # Packages
+  #---------------------------------------------------------------------
+
+  # Packages I always want installed. Most packages I install using
+  # per-project flakes sourced with direnv and nix-shell, so this is
+  # not a huge list.
+  home.packages = [
+    pkgs.bat
+    pkgs.eza
+    pkgs.fd
+    pkgs.fzf
+    pkgs.gh
+    pkgs.htop
+    pkgs.jq
+    pkgs.ripgrep
+    pkgs.watch
+
+  ] ++ (lib.optionals isDarwin [
+    # This is automatically setup on Linux
+    pkgs.cachix
+  ]) ++ (lib.optionals (isLinux && !isWSL) [
+    # Linux dependencies
+  ]);
+}
+
