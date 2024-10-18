823e632de9364b0a9f3423f750b2da8dc59a2ed5
Alkim Ake Gozen <alkimake@pm.me>
2024-10-18 14:55:21 +0900

    TODO: move the default options into a better place

--- apps/os/common/nix.nix
+++ apps/os/common/nix.nix
@@ -16,6 +16,7 @@ in{
       trusted-public-keys = ["ake-nixos-config.cachix.org-1:CiM0xLg1ECceA4PNgDbFCq4tK2xUKdh76cMG5m0y6A0="];
     };
   };
+  #TODO: move to a better place
   options.myNixos = {
     userName = lib.mkOption {
       default = "ake";
